1191767f5032b82d00e9baa0fc7a2a064e742637
Wirris <thenextwill@hotmail.com>
2017-12-30 17:43:51 +0000

    Adding stars via JS

--- js/app.js
+++ js/app.js
@@ -2,6 +2,12 @@
  * A list containing the Stars for the star rating. In this case 3. But can add more easily.
  */
 
+var stars = [
+  "star",
+  "star",
+  "star"
+];
+
 /*
  * A list that contains potential favicon icons
  */
@@ -26,12 +32,21 @@ var cardList = icons.concat(icons);
 var moveCount = 0;
 
 /*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
  * Assigning the star rating to the HTML index.
  */
+
+starTotal();
+function starTotal() {
+  var starHTML = [];
+
+  stars.forEach(function(){
+    starHTML.push($('<li class="star"><i class="fa fa-star"></i></li>'));
+  });
+
+  $('.stars').append(starHTML);
+}
+
+
 /*
  * Using the cardlist and shuffle function we create our random deck.
  * Adding them to the HTML index.
@@ -64,17 +79,6 @@ function shuffle(array) {
   return array;
 }
 
-
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
 var openCard;
 var timer;
 var secondsPassed = 0;
